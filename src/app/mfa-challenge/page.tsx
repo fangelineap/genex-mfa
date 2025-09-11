e56e8907e261786d369c5b4f696bd357b776b796
Fangeline <fangelineap@gmail.com>
2025-09-11 21:57:43 +0700

    feat: add something to mfa page and fix release workflow

--- src/app/mfa-challenge/page.tsx
+++ src/app/mfa-challenge/page.tsx
@@ -86,7 +86,7 @@ export default function MFAChallenge() {
             >
               Unenroll
             </button>
-            <p>Test tagging logic for feat again</p>
+            <p>Test tagging logic for feat 1</p>
           </div>
         </form>
       </div>
