89580b4e5905d9890991ba3327b9f209b204c953
Fangeline <fangelineap@gmail.com>
2025-09-11 22:05:56 +0700

    feat: add something to test release

--- src/app/mfa-challenge/page.tsx
+++ src/app/mfa-challenge/page.tsx
@@ -86,7 +86,7 @@ export default function MFAChallenge() {
             >
               Unenroll
             </button>
-            <p>Test tagging logic for feat 2</p>
+            <p>Test tagging logic for release 0.3.0</p>
           </div>
         </form>
       </div>
