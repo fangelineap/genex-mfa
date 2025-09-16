f5945a01f76134076309930163b3cef43327aa49
Fangeline <89596835+fangelineap@users.noreply.github.com>
2025-09-16 08:52:49 +0700

    Release 0.4.1 (#47)
    * feat: adding some changes
    
    * feat: adding some logic to ci.yml
    
    * feat: adding something for feat test 1
    
    * feat: add smth (#46)
    
    * feat: adding some logic to ci.yml
    
    * feat: adding something for feat test 1
    
    * feat: test 1

--- src/app/mfa-challenge/page.tsx
+++ src/app/mfa-challenge/page.tsx
@@ -86,7 +86,7 @@ export default function MFAChallenge() {
             >
               Unenroll
             </button>
-            <p>Test tagging logic for feat 1</p>
+            <p>Test tagging logic for feat 3</p>
           </div>
         </form>
       </div>
