36acd22bacd42f5cbe243ce2f078324fe5e5bf34
Fangeline <fangelineap@gmail.com>
2025-09-16 09:11:30 +0700

    fix: fixing something

--- src/app/mfa-challenge/page.tsx
+++ src/app/mfa-challenge/page.tsx
@@ -86,7 +86,7 @@ export default function MFAChallenge() {
             >
               Unenroll
             </button>
-            <p>Test tagging logic for feat 3</p>
+            <p>Test tagging logic for feat 2</p>
           </div>
         </form>
       </div>
